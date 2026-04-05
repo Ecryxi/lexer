a78fdf83379ac5c882d76cd52d681fecab898d22
CiroZDP <git@cirozdp.8shield.net>
2026-04-05 15:45:08 +0200

    Use a big array instead of char math in `charutils.h` (#1)
    * feat: add `CHARS` array
    
    * feat: use `CHARS` array to perform checks
    
    * refactor: remove useless functions

--- charutils.h
+++ charutils.h
@@ -1,26 +1,158 @@
 #pragma once
 #include <stdbool.h>
+#include "token.h"
+
+const TokenType CHARS[128] = {
+    Null,
+    
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    Whitespace,
+    
+    Other,
+    StringLiteral,
+    Other,
+    Other,
+    Other,
+    Other,
+    Other,
+    Other,
+    Other,
+    Other,
+    Other,
+    Other,
+    Other,
+    Other,
+    Other,
+
+    IntegerLiteral,
+    IntegerLiteral,
+    IntegerLiteral,
+    IntegerLiteral,
+    IntegerLiteral,
+    IntegerLiteral,
+    IntegerLiteral,
+    IntegerLiteral,
+    IntegerLiteral,
+    IntegerLiteral,
+    
+    Other,
+    Other,
+    Other,
+    Other,
+    Other,
+    Other,
+    Other,
+
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    
+    Other,
+    Other,
+    Other,
+    Other,
+    Identifier,
+    Other,
+
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+    Identifier,
+
+    Other,
+    Other,
+    Other,
+    Other,
+    Whitespace,
+};
 
 static inline bool is_digit(const char c) {
-    return (unsigned) (c - '0') <= 9;
+    return CHARS[c] == IntegerLiteral;
 }
 
-static inline bool is_alpha(const char c) {
-    return (unsigned) ((c | 0x20) - 'a') <= 25;
+static inline bool is_ident_start(const char c) {
+    return CHARS[c] == Identifier;
 }
 
-static inline bool is_alnum(const char c) {
-    return is_digit(c) || is_alpha(c);
-}
-
-inline bool is_ident_start(const uint8_t uc) {
-    return (uc == '_') || is_alpha(uc);
-}
-
-inline bool is_ident(const uint8_t uc) {
-    return (uc == '_') || is_alpha(uc) || is_digit(uc);
+static inline bool is_ident(const char c) {
+    return is_ident_start(c) || is_digit(c);
 }
 
 static inline bool is_space(const char c) {
-    return (unsigned) (c - 1) <= ' '; // Space and below, except NUL, are considered whitespaces
+    return CHARS[c] == Whitespace;
 }

e3b5b1fa3c8583dfc8c13d1a65a92f30550418d3
CiroZDP <git@cirozdp.8shield.net>
2026-04-05 18:04:12 +0200

    refactor: leverage identifier mask factory

--- lexer.c
+++ lexer.c
@@ -104,22 +104,11 @@ void token_new_and_emit(TokenType type, const char* src, size_t *i, size_t span)
 
 
 Token lex_ident(const char* src) {
-    const __m512i TO_UPPER   = _mm512_set1_epi8(~32);
-    const __m512i UNDERSCORE = _mm512_set1_epi8('_');
-    
-    const __m512i ASCII_A    = _mm512_set1_epi8('A');
-    const __m512i POSITION_Z = _mm512_set1_epi8(25); // 25th letter in the alphabet
     Token tok = { .type = Identifier, .lexeme = src, .length = 0 };
 
     for (;; src += 64, tok.length += 64) {
-        const __mmask64 idents = ({
-            const __m512i chunk = _mm512_loadu_epi8(src);
-            const __m512i upper = _mm512_and_si512(chunk, TO_UPPER);
-            const __m512i letterpos = _mm512_sub_epi8(upper, ASCII_A);
-            const __mmask64 letters = _mm512_cmpgt_epu8_mask(letterpos, POSITION_Z);
-
-            letters & _mm512_cmpneq_epu8_mask(chunk, UNDERSCORE);
-        });
+        const __m512i chunk = _mm512_loadu_epi8(src);
+        const __mmask64 idents = get_ident_mask(chunk);
 
         if (__builtin_expect(!idents, 0)) continue;
 

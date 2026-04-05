e95e8d7673ded550b6bd6c836c67f7c8419fe778
CiroZDP <git@cirozdp.8shield.net>
2026-04-05 15:08:43 +0200

    fix: charutils' logic was failing

--- charutils.h
+++ charutils.h
@@ -2,11 +2,11 @@
 #include <stdbool.h>
 
 static inline bool is_digit(const char c) {
-    return (unsigned) (c - '0') <= '9';
+    return (unsigned) (c - '0') <= 9;
 }
 
 static inline bool is_alpha(const char c) {
-    return (unsigned) (c | 0x20 - 'a') <= 'z';
+    return (unsigned) ((c | 0x20) - 'a') <= 25;
 }
 
 static inline bool is_alnum(const char c) {

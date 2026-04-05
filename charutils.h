79603a86b14e25aedc982ee746dcc1d56b4e9661
CiroZDP <git@cirozdp.8shield.net>
2026-04-05 14:09:11 +0200

    refactor: replace uint8_t by char

--- charutils.h
+++ charutils.h
@@ -1,24 +1,22 @@
 #pragma once
-
 #include <stdbool.h>
-#include <stdint.h>
 
-inline bool is_digit(const uint8_t uc) {
-    return (uc - '0') <= '9';
+inline bool is_digit(const char c) {
+    return (unsigned) (c - '0') <= '9';
 }
 
-inline bool is_alpha(const uint8_t uc) {
-    return (uc | 0x20 - 'a') <= 'z';
+inline bool is_alpha(const char c) {
+    return (unsigned) (c | 0x20 - 'a') <= 'z';
 }
 
-inline bool is_alnum(const uint8_t uc) {
-    return is_digit(uc) || is_alpha(uc);
+inline bool is_alnum(const char c) {
+    return is_digit(c) || is_alpha(c);
 }
 
-inline bool is_ident_start(const uint8_t uc) {
-    return (uc == '_') || is_alpha(uc);
+inline bool is_ident_start(const char c) {
+    return (c == '_') || is_alpha(c);
 }
 
-inline bool is_ident(const uint8_t uc) {
-    return (uc == '_') || is_alpha(uc) || is_digit(uc);
+inline bool is_ident(const char c) {
+    return (c == '_') || is_alpha(c) || is_digit(c);
 }

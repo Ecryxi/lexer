cd566a1a3a17c19c865b232a7647f1a041ee1bc6
CiroZDP <git@cirozdp.8shield.net>
2026-04-05 15:05:00 +0200

    fix: make charutils' functions static

--- charutils.h
+++ charutils.h
@@ -1,22 +1,22 @@
 #pragma once
 #include <stdbool.h>
 
-inline bool is_digit(const char c) {
+static inline bool is_digit(const char c) {
     return (unsigned) (c - '0') <= '9';
 }
 
-inline bool is_alpha(const char c) {
+static inline bool is_alpha(const char c) {
     return (unsigned) (c | 0x20 - 'a') <= 'z';
 }
 
-inline bool is_alnum(const char c) {
+static inline bool is_alnum(const char c) {
     return is_digit(c) || is_alpha(c);
 }
 
-inline bool is_ident_start(const char c) {
+static inline bool is_ident_start(const char c) {
     return (c == '_') || is_alpha(c);
 }
 
-inline bool is_ident(const char c) {
+static inline bool is_ident(const char c) {
     return (c == '_') || is_alpha(c) || is_digit(c);
 }
